18e53e5060cb40b28c9362ebced632aa21ad224b
Nick Solonyy <nicksolony@gmail.com>
2019-12-25 18:31:18 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,5 +1,13 @@
 class Meal
+  attr_reader :waiter, :customer
+  attr_accessor :total, :tip
+  @@all = []
   
-  
+  def initialize (waiter, customer, total, tip = 0)
+    @waiter = waiter
+    @customer=customer
+    @total = total
+    @tip = tip
+    @@total << self
   
 end
