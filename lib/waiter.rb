20e69c0b9052b869040fcd1eff9507480f9ab703
Nick Solonyy <nicksolony@gmail.com>
2019-12-25 19:12:02 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -33,6 +33,15 @@ class Waiter
     best_tipped_meal.customer
   end
   
+  def worst_tipper_meal
+    worst_tipped_meal = meals.min do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
+  end
+  
+  end
+  
+  
+  
   
   
   
